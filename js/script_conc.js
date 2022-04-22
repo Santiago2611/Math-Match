abe5923da400b88c1185f3a08faeb765e669c972
MateoZapata1804 <mateozapata1804@gmail.com>
2022-04-21 21:47:07 -0500

    primer juego con avances

--- js/script_conc.js
+++ js/script_conc.js
@@ -1,67 +1,156 @@
+var opPlace = document.getElementById("operacion"); //lugar de la operación
+var ansPlace = document.getElementsByName("ans"); //lugar de las respuestas
+var order; //orden aleatorio en el que saldrán las operaciones
+var opIndex = 0; //índice de la operación actual
+var messageBox = document.getElementById("messageBox"); //campo de texto que dirá si acertó o no
+
 const operations = [
-    "22 + 6",
-    "12 - 4",
-    "80 / 4",
-    "15 + 2",
-    "15 + 5 - 4"
+    ["22 + 6","up"], //operación, dirección de la respuesta correcta
+    ["12 - 4","left"],
+    ["80 / 4","up"],
+    ["15 + 2","right"],
+    ["15 + 5 - 4","down"]
 ];
 
 const answers = [
-    [28,29,23,24],
+    [28,29,23,24], //IMPORTANTE: el orden en el que se imprimen es, respectivamente: --arriba--, --izquierda--, --derecha--, --abajo--
     [12,8,11,9],
     [20,19,10,30],
     [14,16,17,18],
-    [12,19,21,20]
+    [12,19,21,16]
 ];
 
-
-let getRandomOp = function(){
-    var rand = Math.floor(Math.random() * 5);
-    return rand;
-}
-
-let printOpWithAnswers = function(){
-    var opPlace = document.getElementById("operacion");
-    var ansPlace = document.getElementsByName("ans");
-    var randomOp = getRandomOp();
-    opPlace.innerHTML = operations[randomOp];
-    for (let i = 0; i < 4; i++) {
-        ansPlace[i].innerHTML = answers[randomOp][i];
+var getRandomOrder = function(){
+    let order = [], indexToPush = 0;
+    let len = operations.length;
+    for (let i = 0; i < len; i++) {
+        indexToPush = Math.floor(Math.random() * len);
+        if (order.includes(indexToPush) || indexToPush == len) {
+            /*si el índice de la operación ya está en el arreglo, 
+                o el Math.random generó 1 (saldrá error), entonces reinicia el ciclo */
+            i--;
+            continue;
+        } else {
+            order.push(indexToPush);
+        }
     }
-    console.log("showing answers of operation "+opPlace.innerHTML);
+    return order;
 }
 
-let animation = function(arr){
-    var arrows = document.getElementsByName("arrow");
-    arrows[arr].style.color = "green";
+//método que imprime la operacion + respuestas, y activa el listener
+let printOpWithAnswers = function(){ 
+    document.addEventListener("keydown", getPressedArrow); //lo más importante, el listener del teclado, sin esto el juego no funcionaría.
+
+    opPlace.innerHTML = operations[order[opIndex]][0];
+    for (let i = 0; i < 4; i++) {
+        ansPlace[i].innerHTML = answers[order[opIndex]][i];
+    }
+    
+    fadein(opPlace, 500);
+    ansPlace.forEach(element => {
+        fadein(element);
+    });
+}
+
+let animation = function(arr, color){
+    var selectedArrow = document.getElementById(arr);
+    selectedArrow.style.color = color;
+    selectedArrow.style.transition = "none";
     setTimeout(function(){
-        arrows[arr].style.color = "black";
+        selectedArrow.style.color = "rgb(40,40,40)";
+        selectedArrow.style.transition = "1s ease";
     }, 500);
 }
 
-let getPressedArrow = (e) => {
-    var infoBox = document.getElementsByClassName("operationBox")[0];
+let getIfRightAnswer = function(ans){
+    if (ans == operations[order[opIndex]][1]){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let getPressedArrow = (e) => { //método que se ejecutará con el listener
+    var selectedArr = undefined;
+    var accert, color; //booleana de si acertó, color de la flechita
 
     switch (e.keyCode) {
         case 37:
-            infoBox.innerHTML = "izquierda";
-            animation(3);
+            selectedArr = "left";
             break;
         case 38:
-            infoBox.innerHTML = "arriba";
-            animation(0);
+            selectedArr = "up";
             break;
         case 39:
-            infoBox.innerHTML = "derecha";
-            animation(1);
+            selectedArr = "right";
             break;
         case 40:
-            infoBox.innerHTML = "abajo";
-            animation(2);
+            selectedArr = "down";
             break;
         default:
             break;
     }
+
+    /* si se presionó una tecla inválida, la variable selectedArr quedará como undefined,
+    por lo tanto, no entrará al siguiente algoritmo */
+    if (selectedArr != undefined) {
+
+        accert = getIfRightAnswer(selectedArr);
+        if (accert) {
+            document.removeEventListener("keydown", getPressedArrow);
+            console.log("respuesta correcta");
+            color = "green";
+            messageBox.innerHTML = "¡Acertaste!";
+            messageBox.style.color = "green";
+        } else {
+            document.removeEventListener("keydown", getPressedArrow);
+            console.log("respuesta incorrecta :c");
+            color = "red";
+            messageBox.innerHTML = "Respuesta equivocada...";
+            messageBox.style.color = "red";
+        }
+
+        fadeout(messageBox, 2000);
+        fadeout(opPlace, 2500);
+        ansPlace.forEach(element => {
+            fadeout(element, 2500);
+        });
+        
+        opIndex++;
+        (opIndex < operations.length) ? setTimeout(printOpWithAnswers, 3000) : alert("terminaste");
+    }
+    
+    try {
+        animation(selectedArr, color);
+    } catch(e) {
+        console.log("presionaste una tecla no válida");
+    };
+    
 }
 
-document.addEventListener("keydown", getPressedArrow);
+let start = (e) => { //empezar el juego
+    if (e.keyCode == 13) {
+        var op = document.getElementById("operacion");
+        document.removeEventListener("keydown", start);
+        fadeout(op);
+        setTimeout(printOpWithAnswers, 2000);
+        order = getRandomOrder();
+    }
+    console.log("orden: "+order);
+}
+
+let fadeout = function(obj, delay = 0){
+    setTimeout(function(){
+        obj.style.transition = "0.5s ease";
+        obj.style.color = "transparent";
+    }, delay);
+}
+
+let fadein = function(obj, delay = 0){
+    setTimeout(function(){
+        obj.style.transition = "1s ease";
+        obj.style.color = "rgb(40,40,40)";
+    }, delay);
+}
+
+document.addEventListener("keydown", start);
--- views/games/concentrado.php
+++ views/games/concentrado.php
@@ -8,7 +8,7 @@
     <style>
         .container-xl {
             position: relative;
-            background-color: lightblue;
+            background-color: lightgray;
             height: 800px;
             border: dashed black 2px;
             margin-top: 20px;
@@ -17,7 +17,6 @@
 
         .sub-container {
             position: absolute;
-            background-color: red;
             width: 100%;
             height: 50%;
             margin: auto;
@@ -27,7 +26,6 @@
 
         .pad {
             position: relative;
-            background-color: gray;
             font-size: 3em;
             width: 50%;
             height: 100%;
@@ -35,76 +33,100 @@
         }
 
         .arrows {
-            background: lightgray;
             position: relative;
+            background: lightgray;
             font-size: 2em;
-            text-align: center;
+            height: 40%;
             padding: 0px;
+        }
+
+        .fa-arrow-left {
+            font-size: 5em;
+            color: gray;
+            margin: 5px;
+            padding: 5px;
         }
 
         .arrows .fa-caret-up {
             position: absolute;
-            top: 0px;
+            top: -20px;
+            left: 0px;
+            width: 100%;
         }
 
         .arrows .fa-caret-right {
             position: absolute;
+            height: 100%;
+            top: 0px;
             right: 0px;
         }
 
         .arrows .fa-caret-down {
             position: absolute;
-            bottom: 0px;
+            bottom: -20px;
+            left: 0px;
+            width: 100%;
         }
 
         .arrows .fa-caret-left {
-            float: left;
+            position: absolute;
+            height: 100%;
+            top: 0px;
+            left: 0px;
         }
 
-        @keyframes easeAnim {
-            0% {
-                color: green;
-            }
-
-            25% {
-                color: green;
-            }
-
-            100% {
-                color: black;
-            }
+        .operationBox {
+            position: relative;
+            background: white;
+            box-shadow: 0px 5px 10px black;
+            text-align: center;
+            top: 70px;
         }
+
+        #operacion {
+            font-size: 5em;
+        }
+
+        #messageBox {
+            position: relative;
+            top: 100px;
+            text-align: center;
+            color: transparent;
+            transition: color 0.5s ease;
+        }
+
     </style>
 </head>
 <body>
     
     <div class="container-xl">
         <div>
-            <a href=""><i class="">volver</i></a>
+            <a href=""><i class="fas fa-arrow-left"></i></a>
         </div>
         <div class="operationBox">
-            <h2 id="operacion">operacion</h2>
+            <h2 id="operacion">Presiona ENTER para comenzar</h2>
         </div>
+        <h3 id="messageBox"></h3> <!-- comienza con el innerHTML vacío-->
         <div class="sub-container">
             <table class="pad">
                 <tr>
                     <td></td>
-                    <td name="ans">op1</td>
+                    <td name="ans">...</td>
                     <td></td>
                 </tr>
                 <tr>
-                    <td name="ans">op2</td>
+                    <td name="ans">...</td>
                     <td class="arrows">
-                        <i class="fas fa-caret-up" name="arrow"></i>
-                        <i class="fas fa-caret-right" name="arrow"></i>
-                        <i class="fas fa-caret-down" name="arrow"></i>
-                        <i class="fas fa-caret-left" name="arrow"></i>
+                        <i class="fas fa-caret-up" id="up"></i>
+                        <i class="fas fa-caret-right" id="right"></i>
+                        <i class="fas fa-caret-down" id="down"></i>
+                        <i class="fas fa-caret-left" id="left"></i>
                     </td>
-                    <td name="ans">op3</td>
+                    <td name="ans">...</td>
                 </tr>
                 <tr>
                     <td></td>
-                    <td name="ans">op4</td>
+                    <td name="ans">...</td>
                     <td></td>
                 </tr>
             </table>
